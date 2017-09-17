3e9d4afecb543ddae052c79d6ba6d81e3887fa75
Keith Getty <keithgetty@yahoo.com>
2017-09-17 23:25:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeli = [];
+ 
+ function takeANumber(katzDeliLine, name){
+   var line = katzDeliLine;
+   line.push(name);
+   var index = line.indexOf(name)
+   index += 1;
+   return `Welcome, ${name}. You are number ${index} in line.`;
+     }
+ 
