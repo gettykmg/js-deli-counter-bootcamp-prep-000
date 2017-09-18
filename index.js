1c1ff64f19e5b76d927e6e8ce54a2196e9cd78b1
Keith Getty <keithgetty@yahoo.com>
2017-09-18 01:15:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var outcome = "The line is currently: 1. Bill, 2. Jane, 3. Ann"
-  if(katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  } else {
-    for (var i = 0; i < katzDeliLine.length; i++) {
-      var lineNum = i + 1;
-      if (i === (katzDeliLine.length - 1)) {
-        var line2 = `${lineNum}. ${katzDeliLine[i]}`
-        outcome = outcome.concat(line2)
-    }
-    else {var line = `${lineNum}. ${katzDeliLine[i]},`
-    outcome = outcome.concat(line)}
-  }
-}
-return outcome;
-}
+   var outcome = "The line is currently: "
+   if (katzDeliLine.length === 0) {
+     return "The line is currently empty."
+   } else {
+     for (var i = 0; i < katzDeliLine.length; i ++) {
+       var lineNum = i + 1;
+       if (i === (katzDeliLine.length - 1)) {
+         var line2 = `${lineNum}. ${katzDeliLine[i]}`
+         outcome = outcome.concat(line2)
+       }
+        else {var line = `${lineNum}. ${katzDeliLine[i]}, `
+       outcome = outcome.concat(line)}
+     }
+   }
+   return outcome;
+ }
