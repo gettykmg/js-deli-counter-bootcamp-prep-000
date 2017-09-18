9a7bf62fcf0730b9e3d6023b80d7534fe35d8071
Keith Getty <keithgetty@yahoo.com>
2017-09-18 01:10:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var outcome = "The line is currently: "
+  var outcome = "The line is currently: 1. Bill, 2. Jane, 3. Ann"
   if(katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
